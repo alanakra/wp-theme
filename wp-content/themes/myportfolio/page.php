2dc9f96707509226f4e1ab5d0f83eddd3ec9b561
Alan <alanakra@hotmail.fr>
2020-11-20 23:25:46 +0100

    Add pages

--- wp-content/themes/myportfolio/page.php
+++ wp-content/themes/myportfolio/page.php
@@ -2,14 +2,21 @@
 
 <?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
   
-		<article class="projet">
-       <a href="<?php the_permalink(); ?>">
-          <?php the_post_thumbnail(); ?>
-          <h2><?php the_title(); ?></h2>
-          <h3><?php the_excerpt(); ?></h3>
-       </a>  
-		</article>
+<h1 class="titre-page">À propos</h1>
+  
+  <div class="container">
+   <div class="partie-texte">
+     <p class="texte-propos">
+      <?php the_excerpt(); ?>
+     </p>
+   </div>
+    
+   <div class="photo">
+   
+   </div>
 
+  </div>
   <?php endwhile; endif; ?>
 
 <?php get_footer(); ?>
+
