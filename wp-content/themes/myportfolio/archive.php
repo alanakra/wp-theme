2968b4b35210f2bd397371c9b79b733545dfdbf4
Alan <alanakra@hotmail.fr>
2020-11-24 14:16:32 +0100

    Add archives

--- wp-content/themes/myportfolio/archive.php
+++ wp-content/themes/myportfolio/archive.php
@@ -1,3 +1,19 @@
 <?php get_header(); ?>
-	<h1>ARCHIVE</h1>
+<h1 class="titre-page">Mes projets</h1>
+  <p class="definition">Portfolio (nom masculin) \pɔʁt.fo.ljo\ :<br>
+    Dossier personnel dans lequel les acquis de formation et d'expérience d'une personne sont définis et démontrés.</p>
+
+				<section class="container">
+				  <?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
+  
+		      <article class="projet">
+		        <a href="<?php the_permalink(); ?>">
+        	   <?php the_post_thumbnail(); ?>   
+		   	      <h2><?php the_title(); ?></h2>
+      		    <?php the_excerpt(); ?>	
+          </a>            
+		      </article>
+
+	     <?php endwhile; endif; ?>
+				</section>
 <?php get_footer(); ?>
