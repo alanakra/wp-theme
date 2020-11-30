c6c3944db0b3635b727f4e8ece8cd1d1e251b467
Alan <alanakra@hotmail.fr>
2020-11-30 10:27:23 +0100

    Change homepage templating

--- wp-content/themes/myportfolio/front-page.php
+++ wp-content/themes/myportfolio/front-page.php
@@ -2,8 +2,8 @@
 
 <body>
   <div class="supertitre">
-   <h1 class="titre-page">Alan Akra</h1>
-   <h2 class="accroche-first">Développeur web</h2>
+   <h1 class="titre-page"><?php bloginfo('name') ?></h1>
+   <h2 class="accroche-first"><?php bloginfo('description') ?></h2>
    <h3 class="accroche">Bienvenue sur le portfolio d'un jeune <span id="accroche1"></span> <span id="accroche2"></span></h3>
   </div>
 <?php get_footer(); ?>
