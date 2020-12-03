24881b7a3962277302b14986fa6a92b6bb3c020a
Alan <alanakra@hotmail.fr>
2020-12-03 09:41:58 +0100

    First commit in dev branch

--- wp-content/themes/myportfolio/404.php
+++ wp-content/themes/myportfolio/404.php
@@ -2,7 +2,7 @@
 
   <div class="supertitre">
    <h1 class="titre-page">404</h1>
-   <h2 class="accroche-first">Tu t'es perdu ?</h2>
+   <h2 class="accroche-first">Tu t'es perdu ? </h2>
   </div>
 
 <?php get_footer(); ?>
--- wp-content/themes/myportfolio/page-template-homepage.php
+++ wp-content/themes/myportfolio/page-template-homepage.php
@@ -4,6 +4,6 @@ get_header();
 
 <h1><?php get_field('name') ?></h1>
 <h2><?php get_field('description') ?></h2>
-<h3><?php get_field('content') ?></h3>
+<h3><?php get_field('content') ?> <span id="accroche1"><?php get_field('description_list') ?></span> <span id="accroche2"><?php get_field('description_list_2') ?></span></h3>
 
 <?php get_footer() ?>
