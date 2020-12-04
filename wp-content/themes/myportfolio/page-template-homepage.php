fbd6b64b719c1a8e2a2a8d5ea4ea4440c65da337
Alan <alanakra@hotmail.fr>
2020-12-04 09:49:58 +0100

    Rand skill

--- wp-content/themes/myportfolio/page-template-homepage.php
+++ wp-content/themes/myportfolio/page-template-homepage.php
@@ -12,32 +12,33 @@ get_header();
 <?php endif; ?>
 
 <?php if(get_field('content')): ?>
-  <h2 class="accroche"><?php the_field('content') ?></h2>
+  <h2 class="accroche"><?php the_field('content') ?> <?php showSkill() ?></h2>
 <?php endif; ?>
 
 <?php
-
-//Check rows exists.
-if( have_rows('description_list') ):
-
-    // Loop through rows.
-    while( have_rows('description_list') ) : the_row();
-
-        // Load sub field value.
-        $sub_value = get_sub_field('quality');
-        // Do something...
-        echo $sub_value;
-
-    // End loop.
-    endwhile;
-
-// No value.
-else :
-    // Do something...
-endif;
-
+ function showSkill(){
+   //Check rows exists.
+   if( have_rows('description_list') ):
+        $skill = array();
+   
+       // Loop through rows.
+       while( have_rows('description_list') ) : the_row();
+           // Load sub field value.
+           $sub_value = get_sub_field('quality');
+           // Do something...
+         
+           array_push($skill,$sub_value);
+       // End loop.
+       endwhile;
+       $randomInt = rand(0,4);
+       echo($skill[$randomInt]);
+   
+   // No value.
+   else :
+       // Do something...
+   endif;
+ }
 ?>
-
 <?php if(get_field('know_more')): ?>
   <a class="know-more" href="<?php the_field('know_more') ?>">En savoir plus</h2>
 <?php endif; ?>
